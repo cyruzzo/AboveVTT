0cb194993da6835f6888fa155bcc793f3b814052
Mr Kuzio <misterkuzio@gmail.com>
2021-04-19 20:09:28 +1000

    [css-refactoring/mega_importer] Refactoring css from css-in-js to css-in-css

--- ScenesPanel.js
+++ ScenesPanel.js
@@ -975,7 +975,7 @@ function fill_importer(scene_set, start) {
 
 function mega_importer(DDB = false) {
 	container = $("<div id='mega_importer'/>");
-	toggles = $("<div id='importer_toggles'/>");
+	toggles = $("<div id='importer_toggles'/>").css({ height: "30px", width: "100%" });
 
 	if (!DDB) {
 		first = false;
