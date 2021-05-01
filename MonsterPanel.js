0112ee5b0d984dc8e0a321757d20e69b9b65d8dc
Snail Dice <imokadi@gmail.com>
2021-05-02 00:03:04 +0300

    hidden monster Tip that apear on first use only by adding file to local storage when clicking x

--- MonsterPanel.js
+++ MonsterPanel.js
@@ -23,6 +23,56 @@ function init_monster_panel() {
 
 
 		var list = $(event.target).contents().find(".monster-listing__body");
+		// Add helper tooltip (first time only)
+		if (!localStorage.getItem("MonsterPanelTooltipDismissed")) {
+			var markup = `
+				<div
+					id="monster-tooltip"
+					style="
+						background: #fff;
+						border-radius: 3px;
+						border: 15px solid #59b4f3;
+						font-size: 16px;
+						font-weight: bold;
+						margin-bottom: 4px;
+						padding: 10px;
+						text-align: left;
+					"
+				>
+					Did you know?
+					Right-click "+" to add monster as hidden
+					<span
+						class="close"
+						style="
+							cursor: pointer;
+							float: right;
+							font-size: 24px;
+							font-weight: bold;
+							line-height: 24px;
+							padding: 0 5px;
+						"
+					>
+						×
+					</span>
+				</div>
+			`;
+
+			// Add tooltip above the monster list
+			$(list).before(markup);
+			var tooltip = $(event.target).contents().find("#monster-tooltip");
+			console.log("the tooltip", tooltip);
+
+			// Remove tooltip and don't show again
+			if (tooltip) {
+				tooltip.on("click", ".close", function() {
+					localStorage.setItem("MonsterPanelTooltipDismissed", true);
+					$(tooltip).remove();
+				});
+			}
+
+			// To test this:
+			// localStorage.removeItem("MonsterPanelTooltipDismissed")
+		}
 
 
 
