5a26f2afeb72a136e6aa3fb635d6b078d02d403d
kudolpf <bourkeisrobbo@gmail.com>
2021-06-22 23:38:22 +1000

    fix player view breaking when name revealed

--- CombatTracker.js
+++ CombatTracker.js
@@ -126,10 +126,11 @@ function ct_add_token(token,persist=true,disablerolling=false){
 	entry=$("<tr/>");
 	entry.css("height","30px");
 	entry.attr("data-target",token.options.id);	
+	entry.addClass("CTToken");
 	
 	if ((token.options.name) && (window.DM || !token.options.monster || token.options.revealname)) {
 		entry.attr("data-name", token.options.name);
-		entry.addClass("hasTooltip CTToken");
+		entry.addClass("hasTooltip");
 	}
 
 	if(token.options.monster > 0)
