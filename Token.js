06b7787966bc85253d412ee06c930ee9fe832d97
JoshBrodieNZ <5568821+JoshBrodieNZ@users.noreply.github.com>
2021-08-03 22:52:15 +1200

    Wrap condition names in sanitisation logic before writing to tokens to prevent XSS

--- Token.js
+++ Token.js
@@ -339,8 +339,9 @@ class Token {
 			}
 
 			for (let i = 0; i < this.options.custom_conditions.length; i++) {
-				const conditionName = this.options.custom_conditions[i];
-				const conditionSymbolName = conditionName.replaceAll(' ','_').toLowerCase();
+				//Security logic to prevent HTML/JS from being injected into condition names.
+				const conditionName = DOMPurify.sanitize( this.options.custom_conditions[i],{ALLOWED_TAGS: []});
+				const conditionSymbolName = DOMPurify.sanitize( conditionName.replaceAll(' ','_').toLowerCase(),{ALLOWED_TAGS: []});
 				const conditionContainer = $(`<div id='${conditionName}' class='condition-container' />`);
 				let symbolImage;
 				if (conditionName.startsWith('#')) {
