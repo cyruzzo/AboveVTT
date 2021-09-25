fbef6af2ed439853059616fe452621e9232752b5
Jupi-Taru <89948635+Jupi-Taru@users.noreply.github.com>
2021-09-25 15:58:48 -0700

    Jitsi inject only on aboveVTT meetings

--- background.js
+++ background.js
@@ -1,29 +1,4 @@
-/* const add_hide_self_button_code = `let hideSelfButton = $(`<div id='vtt_jitsi_buttons'><button id='jitsi_hide_self_view' class='hasTooltip button-icon jitsi-internal-button' data-name='Hide Self View'><img id='hide_self_view_img' src="${window.EXTENSION_PATH + "assets/hide_self_view.png"}" class='jitsi-internal-image ' /></button></div>`);
-	$("#layout_wrapper").append(hideSelfButton);
-	$("#vtt_jitsi_buttons").css("position", "absolute").css("top", "0px").css("left", "64px")
-	$("#jitsi_hide_self_view").click(
-	function() {
-		let selfView = $("#localVideo_container");
-		let selfViewWrapper = $("#localVideoWrapper");
-		if(selfView.is(":visible"))
-		{
-			selfView.hide();
-			let hiddenPanel = $("<div id='hide_self_view_panel' class='jitsi-self-view-panel'>Self View Hidden</div>");
-			selfViewWrapper.append(hiddenPanel);
-			$("img", this).attr("src", $("#extensionpath").attr('data-path') + "assets/show_self_view.png");
-			$("#jitsi_hide_self_view").attr("data-name","Show Self View");
-			
-		}
-		else
-		{
-			selfView.show();
-			$("#hide_self_view_panel").remove();
-			$("img", this).attr("src", $("#extensionpath").attr('data-path') + "assets/hide_self_view.png");
-			$("#jitsi_hide_self_view").attr("data-name","Hide Self View");
-		}
-	});	` */
-
-const aboveVTT_jitsi_url = "https://meet.jit.si/aboveVTT*";
+const aboveVTT_jitsi_url = "https://meet.jit.si/aboveVTT";
 const jitsi_url = "https://meet.jit.si/*";
 
 function url_domain(data) {
@@ -35,7 +10,7 @@ function url_domain(data) {
 function addJitsiContentScript()
 {
 	chrome.contentScripts.register({
-		matches: [aboveVTT_jitsi_url],
+		matches: [aboveVTT_jitsi_url+"*"],
 		js: [{file: 'LoadJitsi.js'}],
 		allFrames: true
 		});
@@ -45,13 +20,11 @@ function JitsiOncompleteListener(details)
 {
 	if(details.url)
 	{
-		const { hostname } = new URL(details.url);
 		if(details.url.search(aboveVTT_jitsi_url) > -1)
 		{
 			let injectCSSDetails =
 			{
 				file: "abovevtt_jitsi.css",
-				// code: add_hide_self_button_code,
 				frameId: details.frameId				
 			}
 			chrome.tabs.insertCSS(details.tabId, injectCSSDetails);
@@ -60,14 +33,12 @@ function JitsiOncompleteListener(details)
 			let injectJQueryDetails =
 			{
 				file: "jquery.min.js",
-				// code: add_hide_self_button_code,
 				frameId: details.frameId				
 			}
 			chrome.tabs.executeScript(details.tabId, injectJQueryDetails, function() {
 				let injectCodeDetails =
 				{
 					file: "JitsiInternal.js",
-					// code: add_hide_self_button_code,
 					frameId: details.frameId				
 				}
 				
@@ -83,7 +54,7 @@ function addJitsiContentListner()
 {
 	var jitsi_permissions = {
 		permissions:["webNavigation"],
-		origins: [aboveVTT_jitsi_url]
+		origins: [aboveVTT_jitsi_url+"*"]
 	};
 	chrome.permissions.contains(jitsi_permissions, (result) => {
 		if(result)
