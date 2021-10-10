5b54fe4cd4b0f783ee1ef4ff033331a43be4d5f7
cyruzzo <80426829+cyruzzo@users.noreply.github.com>
2021-10-10 23:33:18 +0200

    tentative fix for rubber band ?

--- Token.js
+++ Token.js
@@ -506,7 +506,16 @@ class Token {
 			console.log("trovato!!");
 
 			if (old.css("left") != this.options.left || old.css("top") != this.options.top)
+				
 				remove_selected_token_bounding_box();
+				if(old.is(':animated')){
+					old.stop();
+					old.css({
+						left: this.options.left,
+						top: this.options.top,
+					});
+				}
+				else{
 				old.animate(
 					{
 						left: this.options.left,
@@ -514,6 +523,7 @@ class Token {
 					}, { duration: animationDuration, queue: false, complete: function() {
 						draw_selected_token_bounding_box();
 					} });
+				}
 
 
 			// CONCENTRATION REMINDER
