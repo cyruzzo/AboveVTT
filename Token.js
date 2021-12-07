dd354a0732f1ee2efae8741eecbcea3e347781f5
cyelis <althomas731@gmail.com>
2021-12-07 09:47:45 -0800

    Rework locked and add dmLocked

--- Token.js
+++ Token.js
@@ -108,6 +108,7 @@ class Token {
 	}
 	rotate(newRotation) {
 		if (this.options.locked) return; // don't allow rotation if the token is locked
+		if (this.options.dmLocked) return; // don't allow rotation if the token is locked
 		this.update_from_page();
 		this.options.rotation = newRotation;
 		// this is copied from the place() function. Rather than calling place() every time the draggable.drag function executes, 
@@ -142,6 +143,7 @@ class Token {
 	}
 	move(top, left) {
 		if (this.options.locked) return; // don't allow moving if the token is locked
+		if (this.options.dmLocked) return; // don't allow moving if the token is locked
 		this.update_from_page();
 		this.options.top = top;
 		this.options.left = left;
@@ -664,12 +666,21 @@ class Token {
 				old.find("img").removeClass("token-round");
 			}
 			
-			if(this.options.locked){
+			if(this.options.locked && !window.DM){
 				old.draggable("disable");
 				old.removeClass("ui-state-disabled"); // removing this manually.. otherwise it stops right click menu
 				old.css("z-index", old.css("z-index")-2);
 			}
-			else if(!this.options.locked){
+			else if(!this.options.locked && !window.DM){
+				old.draggable("enable");
+			}	
+
+			if(this.options.dmLocked){
+				old.draggable("disable");
+				old.removeClass("ui-state-disabled"); // removing this manually.. otherwise it stops right click menu
+				old.css("z-index", old.css("z-index")-2);
+			}
+			else if(!this.options.dmLocked){
 				old.draggable("enable");
 			}
 
@@ -1026,6 +1037,27 @@ class Token {
 								}
 							}
 						}
+						for (let id in window.TOKEN_OBJECTS) {
+							if ((id != self.options.id) && window.TOKEN_OBJECTS[id].selected && !window.TOKEN_OBJECTS[id].options.dmLocked) {
+								//console.log("sposto!");
+								var curr = window.TOKEN_OBJECTS[id];
+								var tok = $("#tokens div[data-id='" + id + "']");
+								tok.css('left', (parseInt(curr.orig_left) + offsetLeft) + "px");
+								tok.css('top', (parseInt(curr.orig_top) + offsetTop) + "px");
+								//curr.options.top=(parseInt(curr.orig_top)+offsetTop)+"px";
+								//curr.place();
+
+								const selEl = tok.parent().find("#aura_" + id.replaceAll("/", ""));
+								if (selEl.length > 0) {
+									let currLeft = parseFloat(selEl.attr("data-left"));
+									let currTop = parseFloat(selEl.attr("data-top"));
+									let offsetLeft = Math.round(ui.position.left - parseInt(self.orig_left));
+									let offsetTop = Math.round(ui.position.top - parseInt(self.orig_top));
+									selEl.css('left', (currLeft + offsetLeft) + "px");
+									selEl.css('top', (currTop + offsetTop) + "px");
+								}
+							}
+						}
 
 					}
 
@@ -1033,6 +1065,10 @@ class Token {
 			});
 
 			if(this.options.locked){
+				tok.draggable("disable");
+				tok.removeClass("ui-state-disabled");
+			}
+			if(this.options.dmLocked){
 				tok.draggable("disable");
 				tok.removeClass("ui-state-disabled");
 			}
@@ -1613,6 +1649,13 @@ function token_inputs(opt) {
 			tok.options.locked = false;
 		}
 
+		if (data.token_dmLocked) {
+			tok.options.dmLocked = 1;
+		}
+		else {
+			tok.options.dmLocked = false;
+		}
+
 		if (data.token_disableborder) {
 			tok.options.disableborder = true;
 		}
@@ -1723,13 +1766,27 @@ function token_menu() {
 						delete: { name: 'Delete Token' },
 						token_locked: {
 							type: 'checkbox',
-							name: 'Lock Tokens in Position',
+							name: 'Player Token Lock',
 							events: {
 								click: function(e) {
 									if (e.target == undefined || e.target.checked == undefined) return;
 									$("#tokens .tokenselected").each(function() {
 										id = $(this).attr('data-id');
 										window.TOKEN_OBJECTS[id].options.locked = e.target.checked;
+										window.TOKEN_OBJECTS[id].place_sync_persist();
+									});							
+								}
+							}
+						},
+						token_dmLocked: {
+							type: 'checkbox',
+							name: 'DM Token Lock',
+							events: {
+								click: function(e) {
+									if (e.target == undefined || e.target.checked == undefined) return;
+									$("#tokens .tokenselected").each(function() {
+										id = $(this).attr('data-id');
+										window.TOKEN_OBJECTS[id].options.dmLocked = e.target.checked;
 										window.TOKEN_OBJECTS[id].place_sync_persist();
 									});							
 								}
@@ -1921,8 +1978,13 @@ function token_menu() {
 								},
 								token_locked: {
 									type: 'checkbox',
-									name: 'Lock Token in Position',
+									name: 'Player Token Lock',
 									selected: window.TOKEN_OBJECTS[id].options.locked
+								},
+								token_dmLocked: {
+									type: 'checkbox',
+									name: 'DM Token Lock',
+									selected: window.TOKEN_OBJECTS[id].options.dmLocked
 								},
 								token_disablestat: {
 									type: 'checkbox',
