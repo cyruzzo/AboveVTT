50be3a5392f5895077b9e84bfb8360cb6042b811
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-03-12 22:41:51 -0500

    Missed style removals

--- EncounterHandler.js
+++ EncounterHandler.js
@@ -1101,11 +1101,11 @@ function init_enounter_combat_tracker_iframe() {
 			handles: "all",
 			start: function () {
 				$("#resizeDragMon").each(function (index, element) {
-					var d = $('<div class="iframeResizeCover" style="z-index:99;position:absolute;right:0px;bottom:0px;top:0px;left:0px;width:100%;height:' + $(element).height() + 'px"></div>');
+					var d = $('<div class="iframeResizeCover"></div>');
 					$(element).append(d);
 				});
 				$("#sheet").each(function (index, element) {
-					var d = $('<div class="iframeResizeCover" style="z-index:99;position:absolute;right:0px;bottom:0px;top:0px;left:0px;width:100%;height:' + $(element).height() + 'px"></div>');
+					var d = $('<div class="iframeResizeCover"></div>');
 					$(element).append(d);
 				});
 				$("#resizeDragMon").css("z-index", "10015");
@@ -1121,11 +1121,11 @@ function init_enounter_combat_tracker_iframe() {
 			handles: "all",
 			start: function () {
 				$("#resizeDragMon").each(function (index, element) {
-					var d = $('<div class="iframeResizeCover" style="z-index:99;position:absolute;right:0px;bottom:0px;top:0px;left:0px;width:100%;height:' + $(element).height() + 'px"></div>');
+					var d = $('<div class="iframeResizeCover"></div>');
 					$(element).append(d);
 				});
 				$("#sheet").each(function (index, element) {
-					var d = $('<div class="iframeResizeCover" style="z-index:99;position:absolute;right:0px;bottom:0px;top:0px;left:0px;width:100%;height:' + $(element).height() + 'px"></div>');
+					var d = $('<div class="iframeResizeCover></div>');
 					$(element).append(d);
 				});
 				$("#sheet").css("z-index", "10015");
