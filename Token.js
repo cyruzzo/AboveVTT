9ca1a2870db69a194c095cc915637c0d3b6785b2
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-03-15 00:11:00 -0400

    Dragging tokens through Iframes

--- Token.js
+++ Token.js
@@ -79,6 +79,7 @@ class Token {
 		} else {
 			return false;
 		}
+		return (typeof this.options.monster == "string") && this.options.monster.length > 0
 	}
 
 	size(newsize) {
@@ -930,6 +931,9 @@ class Token {
 			tok.draggable({
 				stop:
 					function (event) {
+						//remove cover for smooth drag
+						$('.iframeResizeCover').remove();
+			
 						// this should be a XOR... (A AND !B) OR (!A AND B)
 						let shallwesnap=  (window.CURRENT_SCENE_DATA.snap == "1"  && !(window.toggleSnap)) || ((window.CURRENT_SCENE_DATA.snap != "1") && window.toggleSnap);
 						console.log("shallwesnap",shallwesnap);
@@ -1028,6 +1032,16 @@ class Token {
 					if(tok.is(":animated")){
 						self.stopAnimation();
 					}
+					
+					// for dragging behind iframes so tokens don't "jump" when you move past it
+					$("#resizeDragMon").each(function (index, element) {
+						var d = $('<div class="iframeResizeCover" style="z-index:99;position:absolute;width:100%;top:0px;left:0px;height:' + $(element).height() + 'px"></div>');
+						$(element).append(d);
+					});
+					$("#sheet").each(function (index, element) {
+						var d = $('<div class="iframeResizeCover" style="z-index:99;position:absolute;width:100%;top:0px;left:0px;height:' + $(element).height() + 'px"></div>');
+						$(element).append(d);
+					});
 
 					console.log("Click x: " + click.x + " y: " + click.y);
 
