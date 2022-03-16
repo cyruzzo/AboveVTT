04eadec04eea3c2033cecf52e6ae9baafda1a100
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-03-16 16:30:54 -0400

    Revert changes - introduced double select again

--- Fog.js
+++ Fog.js
@@ -1188,10 +1188,10 @@ function setup_draw_buttons() {
 			} else {
 				target.css("z-index", "31");
 			}
-			target = $("#fog_overlay, #site");
+			target = $("#fog_overlay, #black_layer");
 
 			if ($(e.target).attr('id') == "measure-button") {
-				target = $("#VTT, #site");
+				target = $("#VTT, #black_layer");
 			}
 
 
