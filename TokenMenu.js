d1226beacd8571bb92cd2160bcf1a883333af7fa
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-04-19 15:05:44 -0400

    Update combat tracker color when border changes

--- TokenMenu.js
+++ TokenMenu.js
@@ -1900,6 +1900,7 @@ function token_context_menu_expanded(tokenIds) {
 		if (e.type === 'change') {
 			tokens.forEach(token => {
 				token.options.color = borderColor;
+				$("#combat_area tr[data-target=" + token.options.id + "] img[class*='Avatar']").css("border-color", borderColor);
 				token.place_sync_persist();
 			});
 		}
@@ -1909,6 +1910,7 @@ function token_context_menu_expanded(tokenIds) {
 				let html = $("#tokens").find(selector);
 				let options = Object.assign({}, token.options);
 				token.options.color = borderColor;
+				$("#combat_area tr[data-target=" + token.options.id + "] img[class*='Avatar']").css("border-color", borderColor);
 				token.place_sync_persist();	
 			});
 		}
