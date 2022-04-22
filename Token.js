93d434abd3fffe134952cfcd1b5d9248239be134
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-04-22 00:57:04 -0400

    Clean up

--- Token.js
+++ Token.js
@@ -1233,9 +1233,9 @@ class Token {
 			});
 
 			tok.mouseup(function (e) {
-			  if (e.which === 3 && (e.ctrlKey || e.metaKey)) {
-			      token_context_menu_expanded([tok.attr("data-id")]);			    
-			  }
+				if (e.which === 3 && (e.ctrlKey || e.metaKey)) {
+				    token_context_menu_expanded([tok.attr("data-id")]);			    
+				}
 			});
 			
 			console.groupEnd()
