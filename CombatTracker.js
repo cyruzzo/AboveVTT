94b111bf51e1187d44a14eed0913955e98988ee3
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-05-16 16:55:42 -0400

    Fix for toggle problem pointed out by Ellasar

--- CombatTracker.js
+++ CombatTracker.js
@@ -253,14 +253,14 @@ function ct_add_token(token,persist=true,disablerolling=false){
 	entry.attr("ishidden", token.options.hidden);
 	entry.addClass("CTToken");
 	
-
-	if(token.options.hidden) {
-		token.options.ct_show = false;
+	if (typeof(token.options.ct_show) == 'undefined'){
+		if(token.options.hidden) {
+			token.options.ct_show = false;
+		}
+		else {
+			token.options.ct_show = true;
+		}	
 	}
-	else {
-		token.options.ct_show = true;
-	}	
-
 
 	if (token.options.ct_show == true || window.DM){
 		if ((token.options.name) && (window.DM || !token.options.monster || token.options.revealname)) {
@@ -341,6 +341,7 @@ function ct_add_token(token,persist=true,disablerolling=false){
 		del=$('<button class="removeTokenCombatButton" style="font-size:10px;"><svg class="delSVG" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg></button>');
 		del.click(
 			function(){
+				token.options.ct_show = 'undefined';
 				ct_remove_token(token);
 				ct_persist();
 			}
