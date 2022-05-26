cb63434002018e4995bd9022545c397263da9b6a
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-05-25 23:34:28 -0400

    Switch server

--- MessageBroker.js
+++ MessageBroker.js
@@ -679,24 +679,7 @@ class MessageBroker {
 				if( (!window.MYSTREAMID))
 					return;
 				const configuration = {
-    				iceServers:  [ {
-					     	urls: "stun:openrelay.metered.ca:80",
-					    },
-					    {
-					      urls: "turn:openrelay.metered.ca:80",
-					      username: "openrelayproject",
-					      credential: "openrelayproject",
-					    },
-					    {
-					      urls: "turn:openrelay.metered.ca:443",
-					      username: "openrelayproject",
-					      credential: "openrelayproject",
-					    },
-					    {
-					      urls: "turn:openrelay.metered.ca:443?transport=tcp",
-					      username: "openrelayproject",
-					      credential: "openrelayproject",
-					    }]
+    				iceServers:  [ {urls: "stun:stun.l.google.com:19302"}]
   				};
 				var peer= new RTCPeerConnection(configuration);
 
@@ -755,24 +738,7 @@ class MessageBroker {
 				if( (!window.MYSTREAMID)  || (msg.data.to!= window.MYSTREAMID) )
 					return;
 				const configuration = {
-    				iceServers:  [ {
-					     	urls: "stun:openrelay.metered.ca:80",
-					    },
-					    {
-					      urls: "turn:openrelay.metered.ca:80",
-					      username: "openrelayproject",
-					      credential: "openrelayproject",
-					    },
-					    {
-					      urls: "turn:openrelay.metered.ca:443",
-					      username: "openrelayproject",
-					      credential: "openrelayproject",
-					    },
-					    {
-					      urls: "turn:openrelay.metered.ca:443?transport=tcp",
-					      username: "openrelayproject",
-					      credential: "openrelayproject",
-					    }]
+    				iceServers:  [ {urls: "stun:stun.l.google.com:19302"}]
   				};
 				var peer= new RTCPeerConnection(configuration);
 
