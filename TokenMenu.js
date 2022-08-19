b47f91987d619dcad4e05dc4ea0d502933eefd8f
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-08-19 13:53:03 -0400

    Remove accidental #643 merge from TokenMenu.js

--- TokenMenu.js
+++ TokenMenu.js
@@ -168,16 +168,12 @@ function token_context_menu_expanded(tokenIds, e) {
 				clickedButton.html(addButtonInternals);
 				tokens.forEach(t =>{
 					t.options.ct_show = undefined;
-					ct_remove_token(t, false);
-					t.place_sync_persist();
+					ct_remove_token(t, false)
 				});
 			} else {
 				clickedButton.removeClass("add-to-ct").addClass("remove-from-ct");
 				clickedButton.html(removeButtonInternals);
-				tokens.forEach(t => {
-					ct_add_token(t, false)
-					t.place_sync_persist();
-				});
+				tokens.forEach(t => ct_add_token(t, false));
 			}
 			ct_persist();
 		});
