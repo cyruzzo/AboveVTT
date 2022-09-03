13503d268a3677e9e0f72cd6479e3593ebbff0a8
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-09-03 04:52:28 -0400

    fix hidden tokens

--- Token.js
+++ Token.js
@@ -1385,11 +1385,11 @@ class Token {
 			}
 			if(window.all_token_objects[this.options.id] == undefined){
 				window.all_token_objects[this.options.id] = {};
+				if (this.options !== undefined){
+					window.all_token_objects[this.options.id] = new Token(this.options);
+				}
 			}
-			if (this.options !== undefined){
-				window.all_token_objects[this.options.id] = new Token(this.options);
-			}
-			else if (typeof window.all_token_objects[this.options.id].options.init !== undefined){		
+			else if (typeof window.all_token_objects[this.options.id].options.init !== undefined && window.all_token_objects[this.options.id].options.ct_show !== undefined){		
 				this.options.init = window.all_token_objects[this.options.id].options.init;
 			}
 			// CONDITIONS
@@ -1976,7 +1976,7 @@ function place_token_at_map_point(tokenObject, x, y) {
 		}
 	}
 	if(window.all_token_objects[options.id] !== undefined){
-		if(window.all_token_objects[options.id].options.ct_show == true){
+		if(window.all_token_objects[options.id].options.ct_show !== undefined){
 			options = window.all_token_objects[options.id].options;
 		  	$(`#combat_area tr[data-target='${options.id}'] .findSVG`).remove();
 	       	let findSVG=$('<svg class="findSVG" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 11c1.33 0 4 .67 4 2v.16c-.97 1.12-2.4 1.84-4 1.84s-3.03-.72-4-1.84V13c0-1.33 2.67-2 4-2zm0-1c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm6 .2C18 6.57 15.35 4 12 4s-6 2.57-6 6.2c0 2.34 1.95 5.44 6 9.14 4.05-3.7 6-6.8 6-9.14zM12 2c4.2 0 8 3.22 8 8.2 0 3.32-2.67 7.25-8 11.8-5.33-4.55-8-8.48-8-11.8C4 5.22 7.8 2 12 2z"/></svg>');	
