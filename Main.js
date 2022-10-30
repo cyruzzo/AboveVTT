d6424518622b9405dc1607db5e65c922871ccfc1
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-10-30 16:39:35 -0400

    remove accidental copy line

--- Main.js
+++ Main.js
@@ -2264,7 +2264,6 @@ function init_character_page_sidebar() {
 			inject_chat_buttons();
 			init_zoom_buttons();
 			monitor_character_sidebar_changes();
-			window.MB.sendMessage('custom/myVTT/syncmeup');
 		}
 	}, 1000);
 }
