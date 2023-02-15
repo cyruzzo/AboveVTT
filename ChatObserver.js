ecf1675c886ff6dc522aca096811d4a1ee795f97
Azmoria <65363489+Azmoria@users.noreply.github.com>
2023-02-15 01:05:34 -0500

    Fix - `/w` whispers

--- ChatObserver.js
+++ ChatObserver.js
@@ -19,7 +19,7 @@ class ChatObserver {
                     return;
                 }
                 let slashCommandMatch = value.match(slashCommandRegex);
-                if (slashCommandMatch?.index === 0) {
+                if (slashCommandMatch?.index === 0 && !value.startsWith("/w ")) {
                     if (self.#parseSlashCommand(value)) {
                         self.#didSubmit(input, value);
                     } else {
