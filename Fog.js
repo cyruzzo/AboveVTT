64dc934459ff2600a5bf7fcfb02644e458b051d4
Azmoria <65363489+Azmoria@users.noreply.github.com>
2023-03-12 21:10:46 -0400

    QoL - Always act as select tool in grid wizard regardless of menu selected

--- Fog.js
+++ Fog.js
@@ -2031,6 +2031,12 @@ function deselect_all_top_buttons(buttonSelectedClasses) {
  * as well as any other selected buttons/required options, such as color/line width, or font family/fontsize
  */
 function get_draw_data(button, menu){
+	if($("#prewiz").length > 0 || $("#wizard_popup").length>0){
+		return {
+			shape:'rect',
+			function:'select'
+		}
+	}
 	if (!$(button).hasClass("menu-option") && !$(button).hasClass("menu-button")){
 		console.groupEnd()
 		return {
