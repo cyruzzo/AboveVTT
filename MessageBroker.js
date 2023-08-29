f36f39b5e295f69200303fa7dd7754e2e0ab583f
Azmoria <65363489+Azmoria@users.noreply.github.com>
2023-08-29 10:19:29 -0400

    Fix - issue with zoom/scroll position on first load of map and loading from storage should remain centered. Note: on first load after patch zoom/scroll position will not match previous position.

--- MessageBroker.js
+++ MessageBroker.js
@@ -1418,14 +1418,9 @@ class MessageBroker {
 
 			$("#VTT").css("--scene-scale", scaleFactor)
 			
-
-
-			apply_zoom_from_storage();
 			reset_canvas();
-
-
-
 			set_default_vttwrapper_size();
+			apply_zoom_from_storage();
 			
 			// WE USED THE DM MAP TO GET RIGH WIDTH/HEIGHT. NOW WE REVERT TO THE PLAYER MAP
 			if(!window.DM && data.dm_map_usable=="1"){
