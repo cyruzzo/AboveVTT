9dcae4e9913099a81be8527cabc101c2fb4912cd
Azmoria <65363489+Azmoria@users.noreply.github.com>
2023-09-10 10:54:07 -0400

    QoL - Save send to default

--- Startup.js
+++ Startup.js
@@ -47,7 +47,13 @@ $(function() {
           // this should never happen because `is_abovevtt_page` covers all the above cases, but cover all possible cases anyway
           throw new Error(`Invalid AboveVTT page: ${window.location.href}`)
         }
-      }).then(() => {      
+      }).then(() => {     
+
+        let lastSendToDefault = localStorage.getItem(`${gameId}-sendToDefault`, gamelog_send_to_text()); 
+
+        if(lastSendToDefault != null){
+          $(`[class*='listItemTextRoot']:contains('${lastSendToDefault}')`).click()
+        }
         window.diceRoller = new DiceRoller();  
         tabCommunicationChannel.addEventListener ('message', (event) => {
           if(event.data.msgType == 'CharacterData' && !find_pc_by_player_id(event.data.characterId, false))
