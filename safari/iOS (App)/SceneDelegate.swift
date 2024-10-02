4ec36641f98584b8e6f9179fc87bff6552955db0
Jeff Senn <jeffsenn@gmail.com>
2024-10-01 21:57:25 -0400

    WIP: initial try for Safari Web Extension

---
+++ safari/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Jeff Senn on 9/29/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
