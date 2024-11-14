fe3ddf06e1d4c52331bd5c84393c88426ff30939
Azmoria <65363489+Azmoria@users.noreply.github.com>
2024-11-14 17:46:29 -0500

    Missed Commit

--- TokensPanel.js
+++ TokensPanel.js
@@ -2301,7 +2301,7 @@ function display_aoe_token_configuration_modal(listItem, placedToken = undefined
     inputWrapper.append(imageScaleWrapper);
 
     let startingOffsetX = customization.tokenOptions.offset?.x || 0;
-    let offsetXWrapper = build_token_num_input(startingOffsetX, false, 'Image Offset X', -100, 100, 5, function (offsetX) {
+    let offsetXWrapper = build_token_num_input(startingOffsetX, false, 'Image Offset X', "", "", 5, function (offsetX) {
 
         if(customization.tokenOptions.offset == undefined)
             customization.tokenOptions.offset = {x: 0, y: 0}
@@ -2312,7 +2312,7 @@ function display_aoe_token_configuration_modal(listItem, placedToken = undefined
     inputWrapper.append(offsetXWrapper);
 
     let startingOffsetY = customization.tokenOptions.offset?.y || 0;
-    let offsetYWrapper = build_token_num_input(startingOffsetY, false, 'Image Offset Y', -100, 100, 5, function (offsetY) {
+    let offsetYWrapper = build_token_num_input(startingOffsetY, false, 'Image Offset Y', "", "", 5, function (offsetY) {
         if(customization.tokenOptions.offset == undefined)
             customization.tokenOptions.offset = {x: 0, y: 0}
         customization.tokenOptions.offset.y = offsetY;
@@ -2331,7 +2331,7 @@ function display_aoe_token_configuration_modal(listItem, placedToken = undefined
     inputWrapper.append(imageZoomWrapper);
 
     let startingOpacity = customization.tokenOptions.imageOpacity || 1;
-    let opacityWrapper = build_token_num_input(startingOpacity, tokens,  'Image Opacity', 0.1, 1, 0.1, function (opacity) {
+    let opacityWrapper = build_token_num_input(startingOpacity, tokens,  'Image Opacity', 0, 1, 0.1, function (opacity) {
         customization.setTokenOption("imageOpacity", opacity);
         persist_token_customization(customization);
         decorate_modal_images(sidebarPanel, listItem, placedToken);
