3660800f3ae638375b560f585cd22cedb253541f
Azmoria <65363489+Azmoria@users.noreply.github.com>
2025-02-23 17:44:39 -0500

    Fix - issue where player img wasn't being picked up due to DDB changes

--- CharactersPage.js
+++ CharactersPage.js
@@ -2250,7 +2250,7 @@ function set_window_name_and_image(callback) {
   window.PLAYER_NAME = $(".ddbc-character-tidbits__heading [class*=ddb-character-app]").text();
   try {
     // This should be just fine, but catch any parsing errors just in case
-    window.PLAYER_IMG = get_higher_res_url($(".ddbc-character-avatar__portrait").css("background-image").slice(4, -1).replace(/"/g, "")) || defaultAvatarUrl;
+    window.PLAYER_IMG = get_higher_res_url($(".ddbc-character-avatar__portrait").css("background-image").slice(4, -1).replace(/"/g, "")) || get_higher_res_url($(".ddbc-character-avatar__portrait").attr('src')) || defaultAvatarUrl;
   } catch {}
 
   if (typeof window.PLAYER_NAME !== "string" || window.PLAYER_NAME.length <= 1 || typeof window.PLAYER_IMG !== "string" || window.PLAYER_IMG.length <= 1) {
