cc8a4bcfb7b0bef0b597270083de7dbc0a687054
Azmoria <65363489+Azmoria@users.noreply.github.com>
2025-10-16 04:23:50 -0400

    Internal - thumbnail fix

--- SidebarPanel.js
+++ SidebarPanel.js
@@ -1584,7 +1584,7 @@ function build_sidebar_list_row(listItem) {
         </g>
     </svg>`)
     }
-    else if ((!isAvttBucketFile && tokenCustomizations?.tokenOptions?.videoToken == true) || ['.mp4', '.webm','.mkv'].some(d => listingImage?.includes(d))){
+    else if (!isAvttBucketFile && (tokenCustomizations?.tokenOptions?.videoToken == true || ['.mp4', '.webm','.mkv'].some(d => listingImage?.includes(d)))){
         img = $(`<video disableRemotePlayback muted src="" loading="lazy" alt="${listItem.name} image" class="token-image video-listing" />`);   
         video = true;
     } else{
