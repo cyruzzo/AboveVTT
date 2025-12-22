eb552d7cea836a973ad0095153fda1badb7baa10
Azmoria <65363489+Azmoria@users.noreply.github.com>
2025-12-21 23:43:26 -0500

    Fix - issue where loading beholder wasn't displaying immediately on clicking a DDB source

--- ScenesHandler.js
+++ ScenesHandler.js
@@ -629,7 +629,6 @@ class ScenesHandler { // ONLY THE DM USES THIS OBJECT
 	}
 
 	build_chapters(keyword, callback) {
-		$('#sources-import-content-container').append(build_combat_tracker_loading_indicator('One moment while we load sourcebook'));
 		let self = this;
 		console.log('scansiono ' + keyword);
 		//let target_list = $("#" + $(event.target).attr('data-target'));
