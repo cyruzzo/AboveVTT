1fe26245666bbfc8a5f5c5c6b8c0f36237eca591
Jeff Senn <jeffsenn@gmail.com>
2026-03-05 12:43:22 -0500

    cleanup

--- Fog.js
+++ Fog.js
@@ -4457,16 +4457,11 @@ function drawing_mouseup(e) {
 			const isCircle = curr.options.tokenStyleSelect == 'circle'
 			const size = curr.options.size * ((isCircle || curr.options.tokenStyleSelect == 'square' || $("#tokens>div[data-id='" + id + "']").hasClass("isAoe")) ? (curr.options.imageSize || 1) : 1);
 			const R = (isCircle ? 0 : curr.options.rotation) || 0;
-			if(! (fullyInside ? isRotatedSquareInsideRect : intersectsRotatedSquare) (
-				{x:x0, y:y0, width:x1-x0, height: y1-y0}, CX, CY, size, R)) continue;
-			
-			// TOKEN IS INSIDE/OVERLAPS THE SELECTION
-			c++;
-			curr.selected = true;
+			curr.selected = (fullyInside ? isRotatedSquareInsideRect : intersectsRotatedSquare) (
+				{x:x0, y:y0, width:x1-x0, height: y1-y0}, CX, CY, size, R);
 		}
 		$("#temp_overlay").css('z-index', '25');
 		draw_selected_token_bounding_box();
-		console.log("READY");
 	}
 	else if (window.DRAWFUNCTION == "measure") {
 		WaypointManager.fadeoutMeasuring(window.PLAYER_ID)
