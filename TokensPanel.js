a9395ead59be5434233af32331bd58bed632dd62
Azmoria <65363489+Azmoria@users.noreply.github.com>
2026-03-17 04:35:55 -0400

    QoL - add an 'add token' button to monster links in notes. Can click/drag from notes created in avtt, for imported source books only click to add is supported from their iframe in app

--- TokensPanel.js
+++ TokensPanel.js
@@ -4801,7 +4801,7 @@ const fetch_and_cache_scene_monster_items = mydebounce( () => {
     });
 });
 
-const fetch_and_cache_monsters = mydebounce( (monsterIds, callback, open5e) => {
+const fetch_and_cache_monsters = mydebounce( (monsterIds, callback=()=>{}, open5e) => {
     if(open5e){
         const cachedIds = Object.keys(cached_open5e_items);
         const monstersToFetch = monsterIds.filter(id => !cachedIds.includes(id) && id != 'customStat');
@@ -4818,7 +4818,6 @@ const fetch_and_cache_monsters = mydebounce( (monsterIds, callback, open5e) => {
             if (response !== false) {
                 update_monster_item_cache(response.map(m => SidebarListItem.Monster(m)), function(){callback()});
             }
- 
         });
     }
     
