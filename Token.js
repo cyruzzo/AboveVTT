81bc2170f18b96c4dd2987afddd90b7010e13ec0
Azmoria <65363489+Azmoria@users.noreply.github.com>
2026-03-23 21:58:37 -0400

    Beta Fix - more syncing issues

--- Token.js
+++ Token.js
@@ -559,7 +559,7 @@ class Token {
 			this.options.left = `${parseFloat(this.options.left) - dx}px`;
 			this.options.top = `${parseFloat(this.options.top) - dy}px`;
 			this.place(0);
-			this.sync($.extend(true, {}, this.options))
+			this.sync()
 		}
 		else{
 			this.place_sync_persist()
@@ -914,7 +914,7 @@ class Token {
 			}
 
 		
-			this.sync($.extend(true, {}, this.options));
+			this.sync();
 		}
 
 
@@ -1272,8 +1272,7 @@ class Token {
 	update_and_sync(e) {
 		self = this;
 		self.update_from_page();
-		if (self.sync != null)
-			self.sync($.extend(true, {}, self.options));//create deep copy so we don't send data when tokens are updated too quickly
+		self.sync();//create deep copy so we don't send data when tokens are updated too quickly
 
 		/* UPDATE COMBAT TRACKER */
 		this.update_combat_tracker()
@@ -3028,33 +3027,6 @@ class Token {
 				});
 
 
-				$("#tokens").append(tok);
-		
-				if(this.options.darkness){
-					let tokenClone = tok.clone();
-					tokenClone.css({
-						left: parseFloat(this.options.left) / window.CURRENT_SCENE_DATA.scale_factor,
-						top: parseFloat(this.options.top) / window.CURRENT_SCENE_DATA.scale_factor,
-						width: `calc(${this.sizeWidth()}px / var(--scene-scale))`,
-						height: `calc(${this.sizeHeight()}px / var(--scene-scale))`
-					})
-			        tokenClone.attr('data-darkness', `darkness_${this.options.id}`);
-			        tokenClone.find('.conditions').remove();
-			        tokenClone.removeClass(['token', 'VTTToken']);
-			        if($(`[data-darkness='darkness_${this.options.id}]'`).length == 0)
-			        	$('#light_container').append(tokenClone);
-			        redraw_drawn_light();
-			    }
-
-			    if(!this.options.id.includes('exampleToken')){
-					this.update_opacity(tok, true);
-			    }
-
-				
-				if(!this.options.id.includes('exampleToken') && !this.options.combatGroupToken){
-					setTokenAuras(tok, this.options);
-					setTokenLight(tok, this.options);
-				}
 
 
 				setTokenBase(tok, this.options);
@@ -3077,7 +3049,7 @@ class Token {
 							window.enable_window_mouse_handlers();
 		
 							if(window.TOKEN_OBJECTS[self.options.id] != undefined){
-								self.sync($.extend(true, {}, self.options));
+								self.sync();
 							}
 							if (window.CURRENT_SCENE_DATA.disableSceneVision == 1 && !window.DM)
 								check_single_token_visibility(self.options.id);
@@ -3090,7 +3062,7 @@ class Token {
 										continue;
 									let curr = window.TOKEN_OBJECTS[id];
 									if (curr != undefined){
-										curr.sync($.extend(true, {}, curr.options));
+										curr.sync();
 										if (curr.options?.darkness === true)
 											darknessMoved = true;
 										if (window.CURRENT_SCENE_DATA.disableSceneVision == 1 && !window.DM)
@@ -3519,6 +3491,34 @@ class Token {
 					},
 					distance: 5,
 				});
+				$("#tokens").append(tok);
+		
+				if(this.options.darkness){
+					let tokenClone = tok.clone();
+					tokenClone.css({
+						left: parseFloat(this.options.left) / window.CURRENT_SCENE_DATA.scale_factor,
+						top: parseFloat(this.options.top) / window.CURRENT_SCENE_DATA.scale_factor,
+						width: `calc(${this.sizeWidth()}px / var(--scene-scale))`,
+						height: `calc(${this.sizeHeight()}px / var(--scene-scale))`
+					})
+			        tokenClone.attr('data-darkness', `darkness_${this.options.id}`);
+			        tokenClone.find('.conditions').remove();
+			        tokenClone.removeClass(['token', 'VTTToken']);
+			        if($(`[data-darkness='darkness_${this.options.id}]'`).length == 0)
+			        	$('#light_container').append(tokenClone);
+			        redraw_drawn_light();
+			    }
+
+			    if(!this.options.id.includes('exampleToken')){
+					this.update_opacity(tok, true);
+			    }
+
+				
+				if(!this.options.id.includes('exampleToken') && !this.options.combatGroupToken){
+					setTokenAuras(tok, this.options);
+					setTokenLight(tok, this.options);
+				}
+
 				let classToClick = null;
 				if(this.isLineAoe()){
 					tok.draggable( "option", "handle", "[data-img]" );
@@ -4622,7 +4622,7 @@ function setTokenLight (token, options) {
 function setTokenBase(token, options) {
 	if(options.imgsrc?.startsWith("class"))
 		return;
-	$(`.token[data-id='${options.id}']>.base`).remove();
+	token.find(`.base`).remove();
 	let base = $(`<div class='base'></div>`);
 
 
@@ -4691,7 +4691,7 @@ function setTokenBase(token, options) {
 		token.toggleClass("freeform", true);
 	}
 	else if(options.tokenStyleSelect === "virtualMiniCircle"){
-		$(`.token[data-id='${options.id}']`).prepend(base);
+		token.prepend(base);
 		//Virtual Mini Circle
 		options.square = true;
 		options.legacyaspectratio = false;
@@ -4699,7 +4699,7 @@ function setTokenBase(token, options) {
 		token.children(".token-image").addClass("preserve-aspect-ratio");
 	}
 	else if(options.tokenStyleSelect === "virtualMiniSquare"){
-		$(`.token[data-id='${options.id}']`).prepend(base);
+		token.prepend(base);
 		//Virtual Mini Square
 		options.square = true;
 		options.legacyaspectratio = false;
