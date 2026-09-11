b7d26cb28bbeb62e4e5d3a9c8d69bc6a32080b00
Azmoria <65363489+Azmoria@users.noreply.github.com>
2026-09-11 16:02:58 -0400

    Fix - issue with darkness aoe no blocking out normal vision when inside the circle; Fix - issue where if the token was lined up with the tangent of the top of a darkness circle it would create incorrect areas of blocked vision
    Internal - additional darkness vision fix missed

--- Fog.js
+++ Fog.js
@@ -8111,7 +8111,7 @@ Ray.prototype.cast = function(boundary) {
 		let u2 = new Vector(u.x - u1.x, u.y - u1.y);
 		let d = Math.hypot(u2.x, u2.y);
 
-		if (d > boundary.radius) {
+		if (d >= boundary.radius) {
 			return;
 		}
 		else {
@@ -9240,8 +9240,8 @@ function draw_aoe_to_canvas(targetAoes, ctx, isDarkness = false){
 
 		let left = parseFloat(targetAoes[i].style.left);
 		let top = parseFloat(targetAoes[i].style.top);
-		let width = parseFloat(targetAoes[i].style.width);
-		let height = parseFloat(targetAoes[i].style.height);
+		let width = targetAoes[i].clientWidth;
+		let height = targetAoes[i].clientHeight;
 		let scale = window.CURRENT_SCENE_DATA.scale_factor != undefined ? window.CURRENT_SCENE_DATA.scale_factor : 1;
 		let halfGrid = window.CURRENT_SCENE_DATA.hpps/2;
 		let divideScale = 1;
@@ -9262,8 +9262,8 @@ function draw_aoe_to_canvas(targetAoes, ctx, isDarkness = false){
 		if(currentAoe.find('.aoe-shape-square').length>0){
 			width = width;
 			height = height;
-			let centerX = (left + width/2)/divideScale;
-			let centerY = (top + height/2)/divideScale;
+			let centerX = (left + width/2);
+			let centerY = (top + height/2);
 
 			let rotationRad = parseFloat(currentAoe.css('--token-rotation')) * (Math.PI/180) 
 
@@ -9276,8 +9276,8 @@ function draw_aoe_to_canvas(targetAoes, ctx, isDarkness = false){
 		if(currentAoe.find('.aoe-shape-line').length>0){
 			width = width;
 			height = height;
-			let centerX = (left + width/2)/divideScale;
-			let centerY = (top + height/2)/divideScale;
+			let centerX = (left + width/2);
+			let centerY = (top + height/2);
 
 			let rotationRad = parseFloat(currentAoe.css('--token-rotation')) * (Math.PI/180) 
 
@@ -9290,8 +9290,8 @@ function draw_aoe_to_canvas(targetAoes, ctx, isDarkness = false){
 		if(currentAoe.find('.aoe-shape-cone').length>0){
 			width = width;
 			height = height;
-			let centerX = (left + width/2)/divideScale;
-			let centerY = (top + height/2)/divideScale;
+			let centerX = (left + width/2);
+			let centerY = (top + height/2);
 
 			let rotationRad = parseFloat(currentAoe.css('--token-rotation')) * (Math.PI/180) 
 
@@ -9306,8 +9306,12 @@ function draw_aoe_to_canvas(targetAoes, ctx, isDarkness = false){
 
 function draw_darkness_aoe_to_canvas(ctx){
 	let darknessAoes = $('[data-darkness]');
-	ctx.globalCompositeOperation='source-over';
+	ctx.save();
+	ctx.globalCompositeOperation = 'destination-out';
+	ctx.globalAlpha = 1;
+	ctx.filter = 'none';
 	draw_aoe_to_canvas(darknessAoes, ctx, true);
+	ctx.restore();
 }
 function clipped_light(auraId, maskPolygon, playerTokenId, canvasWidth = getSceneMapSize().sceneWidth, canvasHeight = getSceneMapSize().sceneHeight, darknessBoundarys = getDarknessBoundarys(), numberOfSharedVisionTokens = 0, tokenWalls = getVisionBlockingTokenWalls()){
 	let visionColor = `rgba(0,0,0,0)`;
